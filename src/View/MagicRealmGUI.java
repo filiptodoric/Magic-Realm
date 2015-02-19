89fb63b792706388c3a8594d0c696a192a3b2d2b
Dylan <Dylan@Dylan-PC>
2015-02-18 20:43:29 -0500

    - barebones optionpane to choose a character (Amazon, Black Knight, Captain, Dwarf, Elf, Swordsman)

--- src/View/MagicRealmGUI.java
+++ src/View/MagicRealmGUI.java
@@ -1,10 +1,14 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class MagicRealmGUI {
 		
-	private JFrame window;
+	private JFrame       window;
+	private JDesktopPane desktopPane;
+	private JButton      startGameButton;
 	
 /****************************************************************************************
 * CONSTRUCTOR
@@ -41,32 +45,19 @@ public class MagicRealmGUI {
 		
 		System.out.println("-- In buildWindow().");
 		
-		// JFrame
 		window = new JFrame("Magic Realm Window");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(1200, 800);
+		desktopPane = new JDesktopPane();
 		
-		// JDesktop Pane: desktopPane1
-		JDesktopPane desktopPane1 = new JDesktopPane();
+		buildMenuBar();
+		buildPlayersInternalFrame();
+		buildMapInternalFrame();
 		
-		// JInternalFrame: internalFrame1
-		JInternalFrame internalFrame1 = new JInternalFrame("Magic Realm Map");
-		internalFrame1.setSize(700, 750);
-		internalFrame1.setMaximizable(true);
-		internalFrame1.setIconifiable(true);
-		internalFrame1.setResizable(true);
-		internalFrame1.setVisible(true);
-		desktopPane1.add(internalFrame1);
-		window.add(desktopPane1);
-		
-		// JScrollPane - holds map
-		JScrollPane mapScrollPane = new JScrollPane();
-		mapScrollPane.setViewportView(getMapLabel());
-		internalFrame1.add(mapScrollPane);
-		
+		window.add(desktopPane);
 		window.setVisible(true);
 	}
-	
+
 	
 	
 	
@@ -83,6 +74,130 @@ public class MagicRealmGUI {
 		JLabel mapImageLabel   = new JLabel(mapImageIcon);
 		mapImageLabel.addMouseListener(new MapBrain(mapImageLabel));
 		return mapImageLabel;
+	}
+
+	
+	
+	
+	
+/****************************************************************************************
+* FUNCTION: buildMenuBar()
+* PURPOSE:  - Adds a menu bar to the window (JFrame).
+****************************************************************************************/
+	public void buildMenuBar() {
+		
+		JMenuBar menuBar  = new JMenuBar();
+		JMenu fileMenu    = new JMenu("File");
+		JMenu networkMenu = new JMenu("Network");
+		JMenu helpMenu    = new JMenu("Help");
+		menuBar.add(fileMenu);
+		menuBar.add(networkMenu);
+		menuBar.add(helpMenu);
+		window.setJMenuBar(menuBar);
+	}
+	
+	
+	
+	
+	
+/****************************************************************************************
+* FUNCTION: buildMapInternalFrame()
+****************************************************************************************/
+	public void buildMapInternalFrame() {
+		
+		// mapScrollPane - holds the mapImageLabel 
+		JScrollPane mapScrollPane = new JScrollPane();
+		mapScrollPane.setViewportView(getMapLabel());
+		
+		// mapInternalFrame - holds the mapScrollPane
+		JInternalFrame mapInternalFrame = new JInternalFrame("Magic Realm Map");
+		mapInternalFrame.setSize(700, 725);
+		mapInternalFrame.setMaximizable(true);
+		mapInternalFrame.setIconifiable(true);
+		mapInternalFrame.setResizable  (true);
+		mapInternalFrame.setVisible    (true);
+		mapInternalFrame.add(mapScrollPane);
+		desktopPane.add(mapInternalFrame);
+	}
+	
+	
+	
+	
+	
+/****************************************************************************************
+* FUNCTION: buildPlayersInternalFrame()
+* PURPOSE:  - Builds the playersInternalFrame along with the components inside.
+****************************************************************************************/
+	public void buildPlayersInternalFrame() {
+		
+		JInternalFrame playersInternalFrame = new JInternalFrame("Players Frame");
+		playersInternalFrame.setSize(450, 200);
+		playersInternalFrame.setLayout(new GridBagLayout());
+		playersInternalFrame.setMaximizable(true);
+		playersInternalFrame.setIconifiable(true);
+		playersInternalFrame.setResizable  (true);
+		playersInternalFrame.setVisible    (true);
+		
+		GridBagConstraints constraints = new GridBagConstraints();
+		
+		/* GridBag Column 1 */
+		constraints.anchor = GridBagConstraints.LINE_START;
+		constraints.fill   = GridBagConstraints.BOTH;
+		constraints.weightx = 0.5;
+		constraints.weighty = 0.5;
+		
+		// "New Character" Button
+		JButton newCharButton = new JButton("New Character");
+		newCharButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				openChooseCharacterDialog();
+			}
+		});
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		playersInternalFrame.add(newCharButton, constraints);
+		
+		// "Start Game" Button
+		startGameButton = new JButton("Start Game");
+		startGameButton.setEnabled(false);
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		playersInternalFrame.add(startGameButton, constraints);
+			
+		desktopPane.add(playersInternalFrame);
+	}
+	
+	
+	
+	
+	
+/****************************************************************************************
+* FUNCTION: openChooseCharDialog()
+* CONTEXT:  - Pressing "New Character" button calls this function.
+****************************************************************************************/
+	public void openChooseCharacterDialog() {
+		
+		System.out.println("-- In newCharButtonClicked()");
+		
+		Object[] characters = { "Amazon", "Black Knight", "Captain", 
+				            "Dwarf",  "Elf", "Swordsman" };
+		
+		String this_character = (String)JOptionPane.showInputDialog(
+		                    	window,
+		                    	"Please choose your character\n",
+		                    	"Choose Character Dialog",
+		                    	JOptionPane.PLAIN_MESSAGE,
+		                    	null,
+		                    	characters,
+		                    	"Amazon");
+
+		if ((this_character != null) && (this_character.length() > 0)) {
+		    System.out.println("You chose " +this_character+ " as your character!");
+		    startGameButton.setEnabled(true);
+		    System.out.println("-- Start Game Button enabled.");
+		    return;
+		}
+		System.out.println("-- Did not choose a character.");
 	}
 	
 }
