403362e731c624ea290ea5930b25dd157346dca8
Filip Todoric <filiptodoric@gmail.com>
2015-02-19 20:02:49 -0500

    added serverConfig class, which has the global var that keeps track of the number of days remaining

---
+++ src/Networking/ServerConfig.java
@@ -0,0 +1,15 @@
+package Networking;
+
+public class ServerConfig {
+
+	// static int which keeps track of the number of days left
+	// users will select all of their actions for the day and 
+	// when they send the command it will decrement the int
+	public static int numberOfDaysRemaining = 28;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
