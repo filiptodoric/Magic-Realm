0ef8d7f46de163809af1ee6d02d7773e4578ea95
Ryan <gr81ape@gmail.com>
2015-04-10 02:18:39 -0400

    Music set up for all monsters!

--- src/ListsAndLogic/MusicLookupTable.java
+++ src/ListsAndLogic/MusicLookupTable.java
@@ -13,5 +13,25 @@ public class MusicLookupTable {
 		table.put("mainTheme", "music/mainTheme.mp3");
 		table.put("combatTheme", "music/combatTheme.mp3");
 		table.put("tradebgm", "music/tradebgm.mp3");
+		table.put("Tremendous Octopus", "music/octopus.mp3");
+		table.put("Tremendous Flying Dragon", "music/dragon.mp3");
+		table.put("Heavy Dragon", "music/dragon.mp3");
+		table.put("Heavy Troll", "music/troll.mp3");
+		table.put("Tremendous Dragon", "music/dragon.mp3");
+		table.put("Heavy Serpent", "music/snakeHiss.mp3");
+		table.put("Giant Bat", "music/bat.mp3");
+		table.put("Giant", "music/troll.mp3");
+		table.put("Heavy Spider", "music/purr.mp3");
+		table.put("Imp", "music/bat.mp3");
+		table.put("Winged Demon", "music/dragon.mp3");
+		table.put("Demon", "music/dragon.mp3");
+		table.put("Goblin with Spear", "music/goblin.mp3");
+		table.put("Goblin with Great Sword", "music/goblin.mp3");
+		table.put("Tremendous Troll", "music/troll.mp3");
+		table.put("Goblin with Axe", "music/goblin.mp3");
+		table.put("Tremendous Spider", "music/purr.mp3");
+		table.put("Heavy Flying Dragon", "music/dragon.mp3");
+		table.put("Ogre", "music/troll.mp3");
+		table.put("Ghost", "music/troll.mp3");
 	}
 }
