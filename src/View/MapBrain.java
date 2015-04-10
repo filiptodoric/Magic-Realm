0d911d9bd4164a4e31573f50af30c74391023948
Ryan <gr81ape@gmail.com>
2015-04-10 00:09:21 -0400

    Custom site and sound chit placement!

--- src/View/MapBrain.java
+++ src/View/MapBrain.java
@@ -369,6 +369,7 @@ public class MapBrain extends MouseAdapter implements Serializable{
 	public void showChits(Clearing thisClearing){
 		
 		System.out.println("-- In showChits().");
+		System.out.println(getCurrentTile().getSiteSoundChit().getName());
 		
 		if(thisClearing.hasChits()){
 			JPanel chitsPanel = new JPanel();
@@ -442,6 +443,15 @@ public class MapBrain extends MouseAdapter implements Serializable{
 				if(clearing.getName().equals(tileText)){
 					return tile;
 				}
+			}
+		}
+		return null;
+	}
+	
+	public HexTile getTileWithTilename(String tileText){
+		for(HexTile tile : tiles){
+			if(tile.getName().equals(tileText)){
+				return tile;
 			}
 		}
 		return null;
